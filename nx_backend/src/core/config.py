c44fd59a99274a175d6cd62dba4b2cb95efc858a
Farrowho <farrowho@gmail.com>
2024-11-07 18:15:30 +0300

    fix/deprecated fastapi events

--- nx_backend/src/core/config.py
+++ nx_backend/src/core/config.py
@@ -14,7 +14,7 @@ REDIS_HOST = os.getenv('REDIS_HOST', '127.0.0.1')
 REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
 
 # Настройки Elasticsearch
-ELASTIC_HOST = os.getenv('ELASTIC_HOST', '127.0.0.1')
+ELASTIC_HOST = os.getenv('ELASTIC_HOST', 'http://127.0.0.1')
 ELASTIC_PORT = int(os.getenv('ELASTIC_PORT', 9200))
 
 # Корень проекта
--- nx_backend/src/main.py
+++ nx_backend/src/main.py
@@ -1,4 +1,5 @@
 from elasticsearch import AsyncElasticsearch
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 from redis.asyncio import Redis
@@ -7,26 +8,26 @@ from api.v1 import films
 from core import config
 from db import elastic, redis
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # setup connections to es and redis on startup
+    redis.redis = Redis(host=config.REDIS_HOST, port=config.REDIS_PORT)
+    elastic.es = AsyncElasticsearch(hosts=[f'{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'])
+
+    yield
+
+    # close connections to es and redis on shutdown
+    await redis.redis.close()
+    await elastic.es.close()
+
+
 app = FastAPI(
     title=config.PROJECT_NAME,
     docs_url='/api/openapi',
     openapi_url='/api/openapi.json',
     default_response_class=ORJSONResponse,
+    lifespan=lifespan
 )
 
-
-@app.lifespan('startup')
-async def startup():
-    redis.redis = Redis(host=config.REDIS_HOST, port=config.REDIS_PORT)
-    elastic.es = AsyncElasticsearch(hosts=[f'{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'])
-
-
-@app.lifespan('shutdown')
-async def shutdown():
-    await redis.redis.close()
-    await elastic.es.close()
-
-
-# Подключаем роутер к серверу, указав префикс /v1/films
-# Теги указываем для удобства навигации по документации
 app.include_router(films.router, prefix='/api/v1/films', tags=['films'])
