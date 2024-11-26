c129c483d6a12ddd9f1c2260448df770181aff34
toth3m00n <alisaageeva311@gmail.com>
2024-11-27 00:42:42 +0300

    feat: добавила заполнение данными один раз за сессию

--- nx_backend/tests/functional/conftest.py
+++ nx_backend/tests/functional/conftest.py
@@ -15,122 +15,119 @@ from functional.testdata.es_indexes import base_index_settings, index_by_name
 from functional.testdata.base_test_rows import base_row_by_name
 
 
-@pytest_asyncio.fixture(scope='session')
+@pytest_asyncio.fixture(scope="session")
 def event_loop():
     loop = asyncio.get_event_loop()
     yield loop
     loop.close()
 
 
-@pytest_asyncio.fixture(name='aiohttp_session', scope='session')
+@pytest_asyncio.fixture(name="aiohttp_session", scope="session")
 async def aiohttp_session():
     session = ClientSession()
     yield session
     await session.close()
 
 
-@pytest_asyncio.fixture(name='es_client', scope='session')
+@pytest_asyncio.fixture(name="es_client", scope="session", autouse=True)
 async def es_client():
     es_client = AsyncElasticsearch(
-        hosts=['{host}:{port}'.format(**ES_CONFIG)],
-        verify_certs=False
+        hosts=["{host}:{port}".format(**ES_CONFIG)], verify_certs=False
     )
+    len_of_prepared_data = 5
+    indexes = ("movies", "genres", "persons")
+
+    for index_name in indexes:
+        base_row: dict = base_row_by_name[index_name]
+        es_data = [base_row]
+
+        for _ in range(len_of_prepared_data - 1):
+            data = copy.deepcopy(base_row)
+            data["_id"] = str(uuid.uuid4())
+            data["id"] = data["_id"]
+            es_data.append(data)
+
+        await es_client.indices.create(
+            index=index_name,
+            settings=base_index_settings,
+            mappings=index_by_name[index_name],
+        )
+
+        _, failed = await async_bulk(
+            client=es_client,
+            actions=es_data,
+            index=index_name,
+            stats_only=True,
+            refresh=True,
+        )
+
+        if failed:
+            raise Exception(
+                f"Ошибка записи данных в Elasticsearch в индексе {index_name}"
+            )
+
+    if not await es_client.indices.exists(index="genres"):
+        raise Exception("НЕ СУЩ genres")
     yield es_client
+
+    for index_name in indexes:
+        await es_client.indices.delete(index=index_name)
+
     await es_client.close()
 
 
-@pytest_asyncio.fixture(name='redis_client', scope='session')
+@pytest_asyncio.fixture(name="redis_client", scope="session")
 async def redis_client():
     redis_client = Redis(**REDIS_CONFIG)
     yield redis_client
     await redis_client.aclose()
 
 
-@pytest_asyncio.fixture(name='es_write_data')
-def es_write_data(es_client: AsyncElasticsearch):
-    async def inner(data: list[dict], index_name: str) -> None:
-
-        if await es_client.indices.exists(index=index_name):
-            await es_client.indices.delete(index=index_name)
-
-        await es_client.indices.create(
-            index=index_name,
-            settings=base_index_settings,
-            mappings=index_by_name[index_name]
-        )
-
-        success, failed = await async_bulk(
-            client=es_client,
-            actions=data,
-            index=index_name,
-            stats_only=True,
-            refresh=True
-        )
-
-        if failed:
-            raise Exception('Ошибка записи данных в Elasticsearch')
-        
-    return inner
-
-
-@pytest_asyncio.fixture(name='make_get_request')
+@pytest_asyncio.fixture(name="make_get_request")
 def make_get_request(aiohttp_session: ClientSession):
     async def inner(api_path: str, params: dict = {}) -> dict:
         response_dict = {}
         url = URL_APP + api_path
 
         async with aiohttp_session.get(url, params=params) as response:
-            response_dict['body'] = await response.json()
-            response_dict['headers'] = response.headers
-            response_dict['status'] = response.status
+            response_dict["body"] = await response.json()
+            response_dict["headers"] = response.headers
+            response_dict["status"] = response.status
 
         return response_dict
 
     return inner
-    
 
-@pytest_asyncio.fixture(name='cache_checkout')
-def cache_checkout(
-    redis_client: Redis,
-    make_get_request
-):
+
+@pytest_asyncio.fixture(name="cache_checkout")
+def cache_checkout(redis_client: Redis, make_get_request):
     async def inner(
-        redis_key: str,
-        api_path: str,
-        key_to_modify: str,
-        request_params: dict = {}
+        redis_key: str, api_path: str, key_to_modify: str, request_params: dict = {}
     ):
         # TODO: разделить фикстуру на две (?)
-        old_response: dict = await make_get_request(
-            api_path=api_path
-        )
+        old_response: dict = await make_get_request(api_path=api_path)
 
-        if old_response['status'] == 404:
-            raise('Записи нет в ES')
+        if old_response["status"] == 404:
+            raise ("Записи нет в ES")
 
-        old_body = old_response['body']
+        old_body = old_response["body"]
 
         if isinstance(old_body, list):
             modified_cache: dict = copy.deepcopy(old_body[0])
-            modified_cache[key_to_modify] = 'SomeRandomRedisTest'
+            modified_cache[key_to_modify] = "SomeRandomRedisTest"
             modified_cache: list[dict] = [modified_cache]
         else:
             modified_cache: dict = copy.deepcopy(old_body)
-            modified_cache[key_to_modify] = 'SomeRandomRedisTest'
-        
-        modified_cache: bytes = json.dumps(modified_cache).encode('utf-8')
+            modified_cache[key_to_modify] = "SomeRandomRedisTest"
+
+        modified_cache: bytes = json.dumps(modified_cache).encode("utf-8")
 
         old_cache: bytes = await redis_client.get(name=redis_key)
 
-        await redis_client.set(
-            name=redis_key,
-            value=modified_cache,
-            ex=60 * 5
-        )
+        await redis_client.set(name=redis_key, value=modified_cache, ex=60 * 5)
 
         new_response: dict = await make_get_request(
-            api_path=api_path,
-            params=request_params
+            api_path=api_path, params=request_params
         )
 
         await redis_client.delete(redis_key)
@@ -138,31 +135,8 @@ def cache_checkout(
         return (
             old_cache,
             modified_cache,
-            json.dumps(old_body).encode('utf-8'),
-            json.dumps(new_response['body']).encode('utf-8'),
-        )
-    
-    return inner
-
-
-@pytest_asyncio.fixture(name='prepare_data_for_es')
-def prepare_data_for_es(es_write_data):
-    async def inner(
-        index_name: str,
-        len_of_prepared_data: int
-    ) -> dict:
-        base_row: dict = base_row_by_name[index_name]
-        es_data = [base_row]
-
-        for _ in range(len_of_prepared_data - 1):
-            data = copy.deepcopy(base_row)
-            data['_id'] = str(uuid.uuid4())
-            data['id'] = data['_id']
-            es_data.append(data)
-        
-        await es_write_data(
-            data=es_data,
-            index_name=index_name
+            json.dumps(old_body).encode("utf-8"),
+            json.dumps(new_response["body"]).encode("utf-8"),
         )
 
     return inner
--- nx_backend/tests/functional/src/test_genres.py
+++ nx_backend/tests/functional/src/test_genres.py
@@ -2,72 +2,52 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    'api_path, redis_key',
+    "api_path, redis_key",
     [
+        ("/api/v1/genres/", "genres_main_"),
         (
-            '/api/v1/genres/',
-            'genres_main_'
+            "/api/v1/genres/5a4d46b8-07ba-4d8f-b376-25ed30944094",
+            "genres_uuid_5a4d46b8-07ba-4d8f-b376-25ed30944094",
         ),
-        (
-            '/api/v1/genres/5a4d46b8-07ba-4d8f-b376-25ed30944094',
-            'genres_uuid_5a4d46b8-07ba-4d8f-b376-25ed30944094'
-        ),
-    ]
+    ],
 )
 @pytest.mark.asyncio
 async def test_genre_redis(
-    prepare_data_for_es,
+    # prepare_data_for_es,
     cache_checkout,
     api_path: str,
-    redis_key: str
+    redis_key: str,
 ):
-    await prepare_data_for_es(
-        index_name='genres',
-        len_of_prepared_data=5
+    old_cache, modified_cache, old_response, new_response = await cache_checkout(
+        redis_key=redis_key, api_path=api_path, key_to_modify="name"
     )
 
-    old_cache, modified_cache, old_response, new_response = await cache_checkout(
-        redis_key=redis_key,
-        api_path=api_path,
-        key_to_modify='name'
-    )
-    
     assert old_cache == old_response
     assert modified_cache == new_response
 
 
 @pytest.mark.asyncio
-async def test_genre_all(
-    make_get_request
-):
-    api_path: str = '/api/v1/genres/'
+async def test_genre_all(make_get_request):
+    api_path: str = "/api/v1/genres/"
 
-    response: dict = await make_get_request(
-        api_path=api_path
-    )
+    response: dict = await make_get_request(api_path=api_path)
 
-    assert response['status'] == 200
-    assert len(response['body']) == 5
+    assert response["status"] == 200
+    assert len(response["body"]) == 5
 
 
 @pytest.mark.parametrize(
-    'uuid_genre, status',
+    "uuid_genre, status",
     [
-        ('5a4d46b8-07ba-4d8f-b376-25ed30944094', 200),
-        ('just_some_random_stuff', 404),
-        ('0a4dbb6c-1fe6-440b-a9bc-b20b7fc37e6c', 404),
-    ]
+        ("5a4d46b8-07ba-4d8f-b376-25ed30944094", 200),
+        ("just_some_random_stuff", 404),
+        ("0a4dbb6c-1fe6-440b-a9bc-b20b7fc37e6c", 404),
+    ],
 )
 @pytest.mark.asyncio
-async def test_genre_specific(
-    make_get_request,
-    uuid_genre: str,
-    status: dict
-):
-    api_path: str = f'/api/v1/genres/{uuid_genre}'
+async def test_genre_specific(make_get_request, uuid_genre: str, status: dict):
+    api_path: str = f"/api/v1/genres/{uuid_genre}"
 
-    response: dict = await make_get_request(
-        api_path=api_path
-    )
+    response: dict = await make_get_request(api_path=api_path)
 
-    assert response['status'] == status
+    assert response["status"] == status
