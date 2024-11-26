b6f3199c363325f4971240aecd5cec2f3dbee9d3
toth3m00n <alisaageeva311@gmail.com>
2024-11-26 03:06:50 +0300

    feat: просолидировала ETL сервис

--- nx_etl/configs/settings.py
+++ nx_etl/configs/settings.py
@@ -1,24 +1,21 @@
-from utils.pydantic_models import EnvSettings
-
-batchsize = 250
-sleep_time_etl = 600
+from configs.setting_model import EnvSettings
 
 settings = EnvSettings()
 
 pg_config = {
-    'dbname': settings.postgres_db,
-    'user': settings.postgres_user,
-    'password': settings.postgres_password,
-    'host': settings.postgres_host,
-    'port': settings.postgres_port,
+    "dbname": settings.postgres_db,
+    "user": settings.postgres_user,
+    "password": settings.postgres_password,
+    "host": settings.postgres_host,
+    "port": settings.postgres_port,
 }
 
 redis_config = {
-    'host': settings.redis_host,
-    'port': settings.redis_port,
+    "host": settings.redis_host,
+    "port": settings.redis_port,
 }
 
 elastic_config = {
-    'host': settings.elastic_host,
-    'port': settings.elastic_port,
+    "host": settings.elastic_host,
+    "port": settings.elastic_port,
 }
